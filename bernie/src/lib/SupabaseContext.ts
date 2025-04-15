ce2830b659baea1162a2fd20d93d827fcf6f40d1
TOUSSENEL Jules <jules.toussenel@epitech.eu>
2025-04-15 14:11:32 +0200

    Delete bernie directory

--- bernie/src/lib/SupabaseContext.ts
+++
@@ -1,5 +0,0 @@
-// src/lib/SupabaseContext.tsx
-import { createContext } from "react";
-import { SupabaseClient } from "@supabase/supabase-js";
-
-export const SupabaseContext = createContext<SupabaseClient | null>(null);
